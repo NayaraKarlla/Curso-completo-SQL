43b79c9d8792d4fd228c870b264f009fd403324b
Nayara Karlla Ferreira <88463152+NayaraKarlla@users.noreply.github.com>
2025-01-31 21:36:36 -0300

    Query módulo 05.sql

--- Query módulo 05.sql
+++ Query módulo 05.sql
@@ -1,4 +1,4 @@
-MÛdulo 5
+M√≥dulo 5
 
 select produto
       ,sum(venda_liquida ) as total_venda
@@ -64,21 +64,21 @@ order
 -----------------
 -- Desafio aula 9
 
--- 1∞ Etapa
+-- 1¬∞ Etapa
 
 select a.venda_liquida
  from vendas_analiticas a 
 
- -- 2∞ Etapa: avaliar a tabela e se tem os dados
+ -- 2¬∞ Etapa: avaliar a tabela e se tem os dados
 
  select fabricante
 	from produtos
-	where fabricante is not null -- descobri que tem 45 fabricantes e as demais linhas s„o null
+	where fabricante is not null -- descobri que tem 45 fabricantes e as demais linhas s√£o null
 
--- 3∞ Etapa: avaliar quais colunas as tabelas tem em comum, uma dica, olhe as chaves prim·rias das tabelas que vai realizar o relacionamento, geralmente o relacionamento se d· pela chave prim·ria e chave estrangeira, porÈm, aqui o relacimento aconteceu pela chave prim·ria e est· tudo certo, ele vai acontecer.
--- Avaliar nas informaÁıes da tabela se na coluna "Nullable" para o campo produto, se est· nulo, se sim, sempre que for registrar uma informaÁ„o na coluna ser· obrigatÛrio informar o cÛdigo do produto, ou seja, nunca havÈra registro de nulo nesse campo. Por esse campo ser assim, vamos usar join no lugar de left join por quest„o de performance.
+-- 3¬∞ Etapa: avaliar quais colunas as tabelas tem em comum, uma dica, olhe as chaves prim√°rias das tabelas que vai realizar o relacionamento, geralmente o relacionamento se d√° pela chave prim√°ria e chave estrangeira, por√©m, aqui o relacimento aconteceu pela chave prim√°ria e est√° tudo certo, ele vai acontecer.
+-- Avaliar nas informa√ß√µes da tabela se na coluna "Nullable" para o campo produto, se est√° nulo, se sim, sempre que for registrar uma informa√ß√£o na coluna ser√° obrigat√≥rio informar o c√≥digo do produto, ou seja, nunca hav√©ra registro de nulo nesse campo. Por esse campo ser assim, vamos usar join no lugar de left join por quest√£o de performance.
 
--- Para validar alguma informaÁ„o È importante fazer um select simples e direto para que pegue os dados fidedignos na consulta, pois aqui n„o haver· nenhum filtro ou agregaÁ„o.
+-- Para validar alguma informa√ß√£o √© importante fazer um select simples e direto para que pegue os dados fidedignos na consulta, pois aqui n√£o haver√° nenhum filtro ou agrega√ß√£o.
 
 select count (*)
  from produtos
@@ -87,7 +87,7 @@ select count (*)
 -- 
 
 select a.fabricante 
-     , coalesce (c.nome, 'n„o identificado') as nome_fabricante
+     , coalesce (c.nome, 'n√£o identificado') as nome_fabricante
      , count (distinct a.produto) as quantidade_produtos
      , avg (b.venda_liquida)      as media_vendas
 from produtos               a             
@@ -101,7 +101,7 @@ Order by media_vendas desc
 -- Desafio aula 11
 
 select b.fabricante 
-     , coalesce (c.nome, 'n„o identificado') as nome_fabricante
+     , coalesce (c.nome, 'n√£o identificado') as nome_fabricante
      , count (distinct b.produto) as quantidade_produtos
      , avg (a.venda_liquida)      as media_vendas
 from vendas_analiticas               a             
@@ -124,7 +124,7 @@ order by coalesce (a.vendedor, b.vendedor)
 ------
 -- Desafio aula 14
 
--- 1∞ Etapa -- aqui ser· possÌvel ver as duas colunas que possuem a mesma informaÁ„o se est„o totalmente preenchidas ou se tem null, e quer dizer que o cliente n„o tem venda
+-- 1¬∞ Etapa -- aqui ser√° poss√≠vel ver as duas colunas que possuem a mesma informa√ß√£o se est√£o totalmente preenchidas ou se tem null, e quer dizer que o cliente n√£o tem venda
 
 select distinct
        a.cliente
@@ -132,17 +132,17 @@ select distinct
 from vendas_analiticas a
 full join entidades    b on a.cliente = b.entidade
 
---2∞ Etapa -- aqui a funÁ„o coalesce fez com que todas as linhas fosse preechidas e n„o tem null 
+--2¬∞ Etapa -- aqui a fun√ß√£o coalesce fez com que todas as linhas fosse preechidas e n√£o tem null 
 
 select coalesce ( a.cliente, b.entidade) as cod_cliente
 from vendas_analiticas a
 full join entidades    b on a.cliente = b.entidade
 
--- 3∞ Etapa -- resultado correto apÛs aula
+-- 3¬∞ Etapa -- resultado correto ap√≥s aula
 
 select coalesce ( a.cliente, b.entidade) as cod_cliente
      , b.nome
-     , isnull (sum (a.venda_liquida), 0) as total_vendido -- se a soma do resultado for null retornar· 0 
+     , isnull (sum (a.venda_liquida), 0) as total_vendido -- se a soma do resultado for null retornar√° 0 
 from vendas_analiticas a
 full join entidades    b on a.cliente = b.entidade
 group by coalesce ( a.cliente, b.entidade )
