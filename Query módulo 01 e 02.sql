8cd1a8b119f1ebae7e5137db9f141b8f079985e9
Nayara Karlla Ferreira <88463152+NayaraKarlla@users.noreply.github.com>
2025-01-31 21:36:08 -0300

    Query módulo 01 e 02.sql

--- Query módulo 01 e 02.sql
+++ Query módulo 01 e 02.sql
@@ -1,6 +1,6 @@
-MÛdulo 1 
+M√≥dulo 1 
 
--- FunÁ„o que tr·s a informaÁ„o da data e hora atual
+-- Fun√ß√£o que tr√°s a informa√ß√£o da data e hora atual
 
 select GETDATE() as data_atual
 
@@ -8,9 +8,9 @@ select GETDATE() as data_atual_02
 
 select GETDATE() as data_atual_03
 
-MÛdulo 2
+M√≥dulo 2
 
--- N˙meros inteiros
+-- N√∫meros inteiros
 
 1. bit (0 e 1)                                                       1 byte
 2. tinyint (0 a 255)                                                 1 byte por registro
@@ -18,38 +18,38 @@ MÛdulo 2
 4. int (-2.147.483.648 a 2.147.483.647)                              4 byte por registro
 5. bigint (-9.223.372.036.854.755.808 a 9.223.372.036.854.775.807)   8 byte por registro
 
--- Valores monet·rios
+-- Valores monet√°rios
 
-1. smallmoney (-214.748,3648 atÈ 214.748,3647)                    4 bytes
-2. money (-922.337.203.685.477,5808 atÈ 922.337.203.685.477,5807) 8 bytes
+1. smallmoney (-214.748,3648 at√© 214.748,3647)                    4 bytes
+2. money (-922.337.203.685.477,5808 at√© 922.337.203.685.477,5807) 8 bytes
 
--- N˙meros com tamanho e precis„o especÌficos
+-- N√∫meros com tamanho e precis√£o espec√≠ficos
 
-1.numeric(t,p) (atÈ 38 dÌgitos) 5 atÈ 17 bytes
-*t = tamaho È a quantidade de dÌgitos que o n˙mero possui
-*p = precis„o È a quantidade de casas decimais 
+1.numeric(t,p) (at√© 38 d√≠gitos) 5 at√© 17 bytes
+*t = tamaho √© a quantidade de d√≠gitos que o n√∫mero possui
+*p = precis√£o √© a quantidade de casas decimais 
 */
 
-exemplo: numeric(3) 255            -- possui 3 dÌgitos
-         numeric(5) 25548          -- possui 5 dÌgitos
-		 numeric(5,2) 25.548,56    -- possui 5 dÌgitos com dus casas decimais
+exemplo: numeric(3) 255            -- possui 3 d√≠gitos
+         numeric(5) 25548          -- possui 5 d√≠gitos
+		 numeric(5,2) 25.548,56    -- possui 5 d√≠gitos com dus casas decimais
 
 -- Valores textuais
 
-1. char ()      (1 atÈ 800 dÌgitos)     1 byte por digito por registro --- usado para textos que tem o tamanho definido e padr„o, exemplo: cpf pois tem 11 dÌgitos definidos
-2. varchar()    (1 atÈ 800 dÌgitos)     1 byte por digito por registro + 1 byte de leitura
-3. varchar(max) (2.140.000.000 dÌgitos) 1 byte por digito por registro + 1 byte 
+1. char ()      (1 at√© 800 d√≠gitos)     1 byte por digito por registro --- usado para textos que tem o tamanho definido e padr√£o, exemplo: cpf pois tem 11 d√≠gitos definidos
+2. varchar()    (1 at√© 800 d√≠gitos)     1 byte por digito por registro + 1 byte de leitura
+3. varchar(max) (2.140.000.000 d√≠gitos) 1 byte por digito por registro + 1 byte 
 
 -- exemplo: varchar (60) -- quer dizer que possui 60 caracteres
 
 -- Valores temporais
 
-1.date         (01/01/0001 atÈ 31/12/9999)                                     
-2.datetime     (01/01/1753 00:00:00.000 atÈ 31/12/9999 23:59:59.999)
-3.datetime2 () (01/01/0001 00:00:00.0000000 atÈ 31/12/9999 23:59:59.99999999)
-4.time ()      (00:00:00.0000000 atÈ 23:59:59.99999999)
+1.date         (01/01/0001 at√© 31/12/9999)                                     
+2.datetime     (01/01/1753 00:00:00.000 at√© 31/12/9999 23:59:59.999)
+3.datetime2 () (01/01/0001 00:00:00.0000000 at√© 31/12/9999 23:59:59.99999999)
+4.time ()      (00:00:00.0000000 at√© 23:59:59.99999999)
 
--- comando para criaÁ„o de banco de dados
+-- comando para cria√ß√£o de banco de dados
 
 create database curso_sql
 
@@ -70,9 +70,9 @@ create table clientes
   ,ativo           bit
 )
 
--- Colocando not null para que n„o seja permitido ausencia de informaÁ„o no campo, ou seja, sempre ser· necess·rio colocar informaÁ„o no campo.
+-- Colocando not null para que n√£o seja permitido ausencia de informa√ß√£o no campo, ou seja, sempre ser√° necess√°rio colocar informa√ß√£o no campo.
 
--- Default È usado para passar a informaÁ„o inicial, posteriormente ela pode mudar.
+-- Default √© usado para passar a informa√ß√£o inicial, posteriormente ela pode mudar.
 
 create table clientes
 (
@@ -88,7 +88,7 @@ create table clientes
 
 )
 
--- comandos com inÌcio drop precisam de atenÁ„o pois geralmente s„o para excluir informaÁ„o
+-- comandos com in√≠cio drop precisam de aten√ß√£o pois geralmente s√£o para excluir informa√ß√£o
 
 drop table clientes
 
@@ -106,11 +106,11 @@ create table clientes
 
 )
 
----sp_help È um comando muito importante do sql server que mostra todas as informaÁıes da tabela
+---sp_help √© um comando muito importante do sql server que mostra todas as informa√ß√µes da tabela
 
 sp_help clientes
 
---- inserindo informaÁıes nos campos que n„o tem informaÁ„o com default
+--- inserindo informa√ß√µes nos campos que n√£o tem informa√ß√£o com default
 
 insert into clientes
 (   nome
@@ -157,21 +157,21 @@ values ('', '85214785210', '07/10/2001', '', 150.00)
 select * 
 from clientes
 
--- chave prim·ria È um ˙nico campo na tabela, ou seja, n„o se repete
--- chave estrangeira È uma chave prim·ria em outra tabela
+-- chave prim√°ria √© um √∫nico campo na tabela, ou seja, n√£o se repete
+-- chave estrangeira √© uma chave prim√°ria em outra tabela
 -- Forma normal
 
--- 1.Ter uma chave prim·ria ou pk (abreviaÁ„o)
--- 2.Valores atÛmicos (valores indivisiveis, que È uma informaÁ„o que pode ficar em uma ˙nica coluna)
--- 3.N„o ter campos multivalorados que s„o duas informaÁıes na mesma colunca
+-- 1.Ter uma chave prim√°ria ou pk (abrevia√ß√£o)
+-- 2.Valores at√≥micos (valores indivisiveis, que √© uma informa√ß√£o que pode ficar em uma √∫nica coluna)
+-- 3.N√£o ter campos multivalorados que s√£o duas informa√ß√µes na mesma colunca
 
 drop table clientes
 
 create table clientes
 (  id_cliente       int           not null primary key identity (1,1)
   ,nome             varchar(60)   not null
-  ,tipo_endereÁo    varchar(20)   not null
-  ,endereÁo         varchar(60)   not null
+  ,tipo_endere√ßo    varchar(20)   not null
+  ,endere√ßo         varchar(60)   not null
   ,numero           numeric(5)    not null
   ,bairro           varchar(60)   not null
   ,complemento      varchar(80)   null
